7e062194418d7cdce488f7f42514cb5250b6c729
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-09-07 20:59:50 +0800

    pages files and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { TestComponent } from './test/test.component';
+import { ResultsComponent } from './results/results.component';
+import { EditTestComponent } from './edit-test/edit-test.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'test', component: TestComponent },
+  { path: 'results', component: ResultsComponent },
+  { path: 'edit-test', component: EditTestComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    TestComponent,
+    ResultsComponent,
+    EditTestComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
