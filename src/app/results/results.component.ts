f65fa216268d0499cff7bc3a24f6b02b1d169184
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-09-09 22:44:02 +0800

    results html

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { PaginationInstance } from 'ngx-pagination';
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.css']
+  styleUrls: ['./results.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ResultsComponent implements OnInit {
+  constructor() {}
+  public config: PaginationInstance = {
+    id: 'custom',
+    itemsPerPage: 10,
+    currentPage: 1,
+  };
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  @Input() id: string = '1';
+  @Input() maxSize: number = 1111;
+  @Output() pageChange: EventEmitter<number> = new EventEmitter();
+  @Output() pageBoundsCorrection: EventEmitter<number> = new EventEmitter();
 }
